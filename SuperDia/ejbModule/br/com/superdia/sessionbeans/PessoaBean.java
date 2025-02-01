a9d898de48eadc15e9ef8e79f5f49b614a29cc54
Vitor Samuel <82065313+VitorSVNascimento@users.noreply.github.com>
2025-02-01 20:48:57 -0300

    create PessoaBean

---
+++ SuperDia/ejbModule/br/com/superdia/sessionbeans/PessoaBean.java
@@ -0,0 +1,44 @@
+package br.com.superdia.sessionbeans;
+
+import java.util.List;
+
+import br.com.superdia.interfaces.IPessoa;
+import br.com.superdia.modelo.Pessoa;
+import jakarta.ejb.Remote;
+import jakarta.ejb.Stateful;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.criteria.CriteriaQuery;
+
+@Stateful
+@Remote(IPessoa.class)
+public class PessoaBean implements IPessoa{
+	
+	@PersistenceContext(unitName = "SuperDia")
+	EntityManager em;
+
+	@Override
+	public void create(Pessoa pessoa) {
+		em.persist(pessoa);
+	}
+
+	@Override
+	public void delete(Pessoa pessoa) {
+		em.remove(em.merge(pessoa));
+	}
+
+	@Override
+	public void update(Pessoa pessoa) {
+		em.merge(pessoa);
+
+	}
+
+	@Override
+	public List<Pessoa> getPessoas() {
+		CriteriaQuery<Pessoa> query = em.getCriteriaBuilder().createQuery(Pessoa.class);
+		query.select(query.from(Pessoa.class));
+		List<Pessoa> pessoas = em.createQuery(query).getResultList();
+		return pessoas;
+	}
+
+}
